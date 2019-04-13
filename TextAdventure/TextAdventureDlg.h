e7e449fc9072d2c5bb941fca4d700023aa6e6641
Sebastian F <ganesu2@gmail.com>
2019-04-13 21:41:48 +0200

    Projektdateien hinzufügen.

---
+++ TextAdventure/TextAdventureDlg.h
@@ -0,0 +1,34 @@
+
+// TextAdventureDlg.h: Headerdatei
+//
+
+#pragma once
+
+
+// CTextAdventureDlg-Dialogfeld
+class CTextAdventureDlg : public CDialogEx
+{
+// Konstruktion
+public:
+	CTextAdventureDlg(CWnd* pParent = nullptr);	// Standardkonstruktor
+
+// Dialogfelddaten
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEXTADVENTURE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV-Unterstützung
+
+
+// Implementierung
+protected:
+	HICON m_hIcon;
+
+	// Generierte Funktionen für die Meldungstabellen
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
